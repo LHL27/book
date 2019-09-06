d9757593b1536a35fed9d76e581f3b17b25a9512
LHL <390341159@qq.com>
2019-09-06 18:49:47 +0800

    第一次上传

---
+++ src/top/hongliankeji/addUser.java
@@ -0,0 +1,18 @@
+package top.hongliankeji;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name="addUser",urlPatterns = {"/addUser"})
+public class addUser extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String userName = request.getParameter("userName");
+        String userPwd = request.getParameter("pwd");
+        System.out.printf(userName + "  "+userPwd);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        this.doPost(request,response);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 林宏连
+  Date: 2019/9/5
+  Time: 18:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>桂林理工大学</title>
+  </head>
+  <body>
+  <form action="addUser" method="post">
+      用户名：<input type="text" name="userName"><br>
+      密码：<input type="password" name="pwd"><br>
+      <input type="submit" value="提交">
+      <input type="reset" value="重置">
+  </form>
+  </body>
+</html>
